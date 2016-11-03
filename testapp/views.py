d06c1f5edc907931a557f3573289524bc53fc389
najeeb <najeeb@globalbridgeinc.net>
2016-11-03 07:26:45 +0000

    2ommit

--- testapp/views.py
+++ testapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect, HttpResponse
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'testapp/template/home.html')
     
 def about(request):
-    return render(request,'about.html')
+    return render(request,'testapp/template/about.html')
